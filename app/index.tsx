14aab0ca288161ba282739825aecc9d1b2742dcc
camilabema1 <camilabema1@gmail.com>
2025-05-04 20:07:46 +0000

    Borrar cosas

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Stack } from 'expo-router';
+import { Text, View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Bienvenida a la app</Text>
+    </View>
+  );
+}
